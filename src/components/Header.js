9da8aedc27e7d1ad65cf81265fa869b2fe00996f
Maureen Anyanwu <maureenanyanwu519@gmail.com>
2021-10-31 15:36:12 +0100

    header fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ export default class Header extends Component {
     
     render() {
         return (
-            <div className="header flex justify-between px-10 py-3 text-base text-green-900">
+            <div className="header flex justify-between px-10 py-3 text-base text-green-500">
                 <Link to="/">    
                     <div className="flex cursor-pointer"> <GiThink/>&nbsp; 
                         <p className="font-cursive">Decipher</p>
                     </div>
                 </Link>
                 <Link to="/all-questions">
-                    <p className="hover:text-green-400 cursor-pointer">See all questions</p>
+                    <p className="hover:text-red-500 cursor-pointer">See all questions</p>
                 </Link>
             </div>
         )
